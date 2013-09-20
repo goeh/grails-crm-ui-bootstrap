660442617ce3316e194677c7ca550af98aa60628
Goran Ehrsson <goran@technipelago.se>
2013-09-20 21:38:35 +0200

    Version 1.2.1

--- CrmUiBootstrapGrailsPlugin.groovy
+++ CrmUiBootstrapGrailsPlugin.groovy
@@ -16,14 +16,10 @@
 
 class CrmUiBootstrapGrailsPlugin {
     def groupId = "grails.crm"
-    // the plugin version
-    def version = "1.2.0"
-    // the version or versions of Grails the plugin is designed for
+    def version = "1.2.1"
     def grailsVersion = "2.2 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
     def loadAfter = ['crmCore']
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
             "grails-app/conf/ApplicationResources.groovy",
