3cf299b9a9f129a0845833155c2c3eccafafa3b2
Goran Ehrsson <goran@technipelago.se>
2013-11-30 20:21:06 +0100

    prettytime can now take locale attribute as String or Locale

--- test/unit/grails/plugins/crm/ui/bootstrap/PrettyTimeTests.groovy
+++ test/unit/grails/plugins/crm/ui/bootstrap/PrettyTimeTests.groovy
@@ -8,9 +8,12 @@ class PrettyTimeTests {
 
     @Test
     void testOneDayAgo() {
-        assert applyTemplate('<crm:prettyTime date="\${date}"/>', [date:new Date() - 1]).trim() == "1 day ago"
-        request.addPreferredLocale(new Locale('sv_SE'))
-        assert applyTemplate('<crm:prettyTime date="\${date}"/>', [date:new Date() - 1]).trim() == "1 dag sedan"
-        assert applyTemplate('<crm:prettyTime date="\${date}"/>', [date:new Date() - 2]).trim() == "2 dagar sedan"
+        assert applyTemplate('<crm:prettyTime date="\${date}" locale="\${locale}"/>', [locale: new Locale('en'), date: new Date() - 1]).trim() == "1 day ago"
+
+        assert applyTemplate('<crm:prettyTime date="\${date}" locale="\${locale}"/>', [locale: new Locale('sv'), date: new Date() - 1]).trim() == "1 dag sedan"
+        assert applyTemplate('<crm:prettyTime date="\${date}" locale="sv"/>', [date: new Date() - 2]).trim() == "2 dagar sedan"
+
+        assert applyTemplate('<crm:prettyTime date="\${date}" locale="\${locale}"/>', [locale: new Locale('fi'), date: new Date() - 1]).trim() == "eilen"
+        assert applyTemplate('<crm:prettyTime date="\${date}" locale="fi"/>', [date: new Date() - 2]).trim() == "2 päivää sitten"
     }
 }
