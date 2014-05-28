a3f9a7feecb5a32e6280b70a485a21765fa4d4bd
Goran Ehrsson <goran@technipelago.se>
2014-05-28 09:02:46 +0200

    Upgrade to Grails 2.4 - first attempt

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,52 +1,60 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target"
 grails.project.target.level = 1.6
 
 grails.project.repos.default = "crm"
 
+grails.project.fork = [
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
-    inherits("global") {}
+    inherits("global") {
+      excludes 'ehcache-core'
+    }
     log "warn"
-    legacyResolve false
     repositories {
         grailsCentral()
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
         mavenRepo "http://labs.technipelago.se/repo/plugins-releases-local/"
         mavenCentral()
+        mavenLocal()
     }
     dependencies {
-        compile "org.ocpsoft.prettytime:prettytime:3.2.1.Final"
-        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+        compile "org.ocpsoft.prettytime:prettytime:3.2.5.Final"
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-                ":release:2.2.1",
+        build(":release:3.0.1",
                 ":rest-client-builder:1.0.3") {
             export = false
         }
-        test(":hibernate:$grailsVersion") {
+        test(":hibernate:3.6.10.15") {
             export = false
         }
 
-        test(":spock:0.7") {
-            export = false
-            exclude "spock-grails-support"
-        }
-        test(":codenarc:0.19") { export = false }
+        test(":codenarc:0.21") { export = false }
         test(":code-coverage:1.2.7") { export = false }
 
-        runtime ":resources:1.2.7"
+        runtime ":resources:1.2.8"
         runtime ":less-resources:1.3.3.2"
-        runtime ":jquery:1.10.2"
+        runtime ":jquery:1.11.1"
         runtime ":twitter-bootstrap:2.3.2"
-        runtime ":fields:1.3"
-        runtime ":navigation:1.3.2"
+        runtime ":fields:1.3.1-SNAPSHOT"
+        runtime ":navigation:1.3.3-SNAPSHOT"
         runtime ":famfamfam:1.0.1"
         runtime ":content-buffer:1.0.1"
 
-        compile "grails.crm:crm-core:latest.integration"
-        runtime "grails.crm:crm-notification:latest.integration"
-        runtime "grails.crm:crm-security:latest.integration"
+//        compile "grails.crm:crm-core:latest.integration"
+//        runtime "grails.crm:crm-notification:latest.integration"
+//        runtime "grails.crm:crm-security:latest.integration"
 
         runtime ":recent-domain:latest.integration"
         runtime ":user-tag:latest.integration"
@@ -56,6 +64,10 @@ grails.project.dependency.resolution = {
         }
     }
 }
+
+grails.plugin.location.'crm-core' = '../crm-core'
+grails.plugin.location.'crm-notification' = '../crm-notification'
+grails.plugin.location.'crm-security' = '../crm-security'
 
 codenarc {
     reports = {
