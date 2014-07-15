cacc9c4f7c4340608dc68842db4fd7950e3fb597
Goran Ehrsson <goran@technipelago.se>
2014-07-15 22:10:40 +0200

    Version 1.2.14

--- CrmUiBootstrapGrailsPlugin.groovy
+++ CrmUiBootstrapGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmUiBootstrapGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.2.13"
+    def version = "1.2.14"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore']
