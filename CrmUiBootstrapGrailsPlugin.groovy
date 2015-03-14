2bee3bce6a84b07a901caf1af21d033889d3b982
Goran Ehrsson <goran@technipelago.se>
2015-03-14 17:25:46 +0100

    Version 2.4.0

--- CrmUiBootstrapGrailsPlugin.groovy
+++ CrmUiBootstrapGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmUiBootstrapGrailsPlugin {
     def groupId = ""
-    def version = "2.4.0-SNAPSHOT"
+    def version = "2.4.0"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore']
