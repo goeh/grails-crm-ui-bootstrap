3f495572071b44fad0eaa4feb1957e4ae8858c5b
Goran Ehrsson <goran@technipelago.se>
2016-08-14 19:31:02 +0200

    Upgraded dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -28,7 +28,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:3.0.1", ":rest-client-builder:1.0.3") {
+        build(":release:3.1.2", ":rest-client-builder:2.1.1") {
             export = false
         }
         test(":hibernate4:4.3.6.1") {
@@ -36,7 +36,7 @@ grails.project.dependency.resolution = {
             export = false
         }
 
-        test(":codenarc:0.24.1") { export = false }
+        test(":codenarc:0.25.2") { export = false }
         test(":code-coverage:2.0.3-3") { export = false }
 
         test(":selection-repository:0.9.3") {
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
         compile ":twitter-bootstrap:2.3.2.2"
         compile ":fields:1.4"
 
-        compile ":crm-security:2.4.2"
+        compile ":crm-security:2.4.3"
 
         compile ":recent-domain:0.6.0"
         compile ":user-tag:1.0"
