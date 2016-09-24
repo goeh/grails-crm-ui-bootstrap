ec53cbd257c84ae8360238a812e947934d8109ae
Goran Ehrsson <goran@technipelago.se>
2016-09-24 23:33:49 +0200

    Next snapshot version

--- CrmUiBootstrapGrailsPlugin.groovy
+++ CrmUiBootstrapGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmUiBootstrapGrailsPlugin {
     def groupId = ""
-    def version = "2.4.2"
+    def version = "2.4.3-SNAPSHOT"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore']
